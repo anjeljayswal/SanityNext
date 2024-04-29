0fdcf03da5693d3e0abdd4e75c1cdccdf2063233
anjeljayswal <anjeljayswal@gmail.com>
2024-04-29 08:51:42 +0530

    id changed

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -16,7 +16,9 @@
 // ./src/utils/sanity/client.ts
 import {createClient} from 'next-sanity'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "pv8y60vp"
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "df7tslvm"
+const projectId = "df7tslvm"
+
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
