9d4b70103e75169dce3b5f96fba7fd23018899dc
anjeljayswal <anjeljayswal@gmail.com>
2024-04-29 09:18:06 +0530

    dataset name

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -19,7 +19,7 @@ import {createClient} from 'next-sanity'
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "df7tslvm"
 const projectId = "df7tslvm"
 
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
+const dataset = "production" // "production"
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
 export const client = createClient({
